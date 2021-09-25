d57cd83dc5b8451605be63ae5335c390e97a2115
Nicolas Marier <software@nmarier.com>
2021-09-25 18:12:39 -0400

    refactor(extract-content-info): refactor date parsing

--- extract_content_info.py
+++ extract_content_info.py
@@ -9,19 +9,6 @@ from audio_video_content import AudioVideoContent
 from content_type import ContentType
 
 
-def get_content_start_date_from_xml(xml_path: Path) -> datetime:
-    xml_info = ET.parse(xml_path)
-    time_info_array = [e for e in xml_info.getroot().findall("Message") if e.find("Array")][0].find("Array")
-
-    for elm in time_info_array:
-        try:
-            # Not sure if the month is zero-padded
-            return datetime.strptime(elm.text, "%a %b %d %H:%M:%S %Y")
-        except ValueError:
-            pass
-    raise Exception(f"Could not determine content start for file '{xml_path}'")
-
-
 def get_content_with_xml_info(content_path: Path, content_type: ContentType) -> AudioVideoContent:
     filename = content_path.stem
     xml_info_file = content_path.parent / f"{filename}.xml"
@@ -29,6 +16,28 @@ def get_content_with_xml_info(content_path: Path, content_type: ContentType) -> 
     duration = timedelta(seconds=float(ffmpeg.probe(content_path)["format"]["duration"]))
 
     return AudioVideoContent(path=content_path, content_type=content_type, start_date=start_date, duration=duration)
+
+
+def get_content_start_date_from_xml(xml_path: Path) -> datetime:
+    xml_info = ET.parse(xml_path)
+    time_info_array = [e for e in xml_info.getroot().findall("Message") if e.find("Array")][0].find("Array")
+
+    for elm in time_info_array:
+        try:
+            return parse_date(elm.text)
+        except ValueError:
+            pass
+    raise Exception(f"Could not determine content start for file '{xml_path}'")
+
+
+def parse_date(date: str) -> datetime:
+    try:
+        # Not sure if the month is zero-padded
+        return datetime.strptime(date, "%a %b %d %H:%M:%S %Y")
+    except ValueError:
+        pass
+
+    raise ValueError("The provided string does not look like a date")
 
 
 def get_all_content(connect_dir: Path) -> List[AudioVideoContent]:
